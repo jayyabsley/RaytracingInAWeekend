de42a73edb03a86137408f254ccc9b8071452fe0
Jay Yabsley <jay.yabsley@aie.edu.au>
2019-11-04 12:32:46 +1100

    Init commit, moving to another PC

---
+++ RaytracingInAWeekend/RaytracingInAWeekend.cpp
@@ -0,0 +1,33 @@
+// RaytracingInAWeekend.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include "vec3.h"
+#include <iostream>
+
+int main()
+{
+	int nx = 200;
+	int ny = 100;
+	std::cout << "P3\n" << nx << " " << ny << "\n255\n";
+	for (int j = ny - 1; j >= 0; j--) {
+		for (int i = 0; i < nx; i++) {
+			vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2f);
+			int ir = int(255.99*col[0]);
+			int ig = int(255.99*col[1]);
+			int ib = int(255.99*col[2]);
+			std::cout << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
